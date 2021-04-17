45507f9a3306929e991d0a29cc45039a4c2fdbfe
IrleySilvestre <66978110+IrleySilvestre@users.noreply.github.com>
2021-04-17 01:27:53 -0300

    Initial Comit

---
+++ src/components/PostCard/index.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export const PostCard = (props)=>{
+    return(
+        <div className="post">
+            <img src={props.cover} alt={props.title}/>
+
+            <div className="post-content">
+                <h1>{props.title}</h1>
+                <p>{props.body}</p>
+            </div>
+
+        </div>
+    )
+}
